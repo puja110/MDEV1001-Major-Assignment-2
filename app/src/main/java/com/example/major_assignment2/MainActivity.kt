948af6f579feb5bfe25bb62837b87f7da9663436
puja110 <csta.puja@gmmail.com>
2023-11-21 08:21:50 -0500

    MoviesFragment added inside MainActivity class

--- app/src/main/java/com/example/major_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/major_assignment2/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = MoviesFragment.getInstance()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragment, fragment.javaClass.simpleName)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/major_assignment2/MoviesFragment.kt
@@ -0,0 +1,28 @@
+package com.example.major_assignment2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MoviesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movies, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun getInstance(): Fragment {
+            return MoviesFragment()
+        }
+    }
+}
